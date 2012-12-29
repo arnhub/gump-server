1332818201d7e000ee47699e63e71e94f83a3744
Hubert SABLONNIÈRE <hubert.sablonniere@gmail.com>
2012-12-29 18:45:26 +0100

    Adding static example for game and URL access for it

--- server.js
+++ server.js
@@ -1,14 +1,32 @@
 'use strict';
 
-var nodeStatic   = require('node-static'),
-    http         = require('http'),
-    staticServer = new nodeStatic.Server('./gump-app/app'),
-    port         = 8080;
+var nodeStatic = require('node-static'),
+    http = require('http'),
+    staticServer = new nodeStatic.Server('./gump-app/dist'),
+    port = 8080,
+    game;
+
+game = {
+  status: 'server-confirmed',
+  server: 'player1',
+  player1: {
+    firstName: 'Hubert',
+    lastName : 'SABLONNIERE',
+    email    : 'hubert.sablonniere@gmail.com',
+    score    : 8
+  },
+  player2: {
+    firstName: 'Arnaud',
+    lastName : 'SPANNEUT',
+    email    : 'arnaud.spanneut@gmail.com',
+    score    : 11
+  }
+};
 
 http.createServer(function (request, response) {
-  
-  if (request.url.match(/^\/api/)) {
-    response.end('foobar');
+
+  if (request.url.match(/^\/api\/game/)) {
+    response.end(JSON.stringify(game));
   } else {
     staticServer.serve(request, response);
   }
