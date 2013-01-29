340b352017d3663273f363b746d18ed2e8eb76ff
Hubert SABLONNIÈRE <hubert.sablonniere@gmail.com>
2013-01-29 13:33:26 +0100

    Refactoring game initialization and status types

--- model/game.js
+++ model/game.js
@@ -1,34 +1,42 @@
 'use strict';
 
-var generateKey = function () {
-      return (Math.random() * 1e36).toString(36);
-    },
-    game = {
-      status: 'server-confirmed',
-      server: 'player1',
-      player1: {
-        firstName: '?',
-        lastName: '',
-        email: '',
-        score: 0,
-        connected: false,
-        key: ''
-      },
-      player2: {
-        firstName: '?',
-        lastName: '',
-        email: '',
-        score: 0,
-        connected: false,
-        key: ''
-      }
-    };
+var player,
+    game;
+
+player = {
+  init    : function () {
+    this.firstName = '?';
+    this.lastName = '';
+    this.email = '';
+    this.score = 0;
+    this.connected = false;
+    this.resetKey();
+    return this;
+  },
+  resetKey: function () {
+    this.key = (Math.random() * 1e36).toString(36);
+  },
+  reset   : function (firstName, lastName, email) {
+    this.score = 0;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.email = email;
+  }
+};
+
+game = {
+  SERVER_UNKNOWN    : 0,
+  SERVER_UNCONFIRMED: 1,
+  SERVER_CONFIRMED  : 2,
+  server            : null,
+  player1           : Object.create(player).init(),
+  player2           : Object.create(player).init(),
+  init              : function () {
+    this.status = this.SERVER_UNKNOWN;
+    return this;
+  }
+}.init();
 
 exports.get = function () {
   return game;
-};
-
-exports.reset = function () {
-  game.player1.key = generateKey();
-  game.player2.key = generateKey();
 };
